0cf3ddf4ecc6ca66711da56ba138ff2221dd7187
emzi20 <mziemichod@wp.pl>
2019-03-12 19:23:02 +0100

    first comit

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>CV</title>
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <link rel="Stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+       <nav>
+          <ul id= "menu">
+            <li><a href="#aboutMe"> O mnie</a><li>
+            <li><a href="#education">Wykształcenie</a><li>
+            <li><a href="#skills">Umiejętności</a><li>
+            <li><a href="#passion">Pasja</a><li>
+            <li><a href="#contact">Kontakt</a><li>
+          </ul>
+        </nav>
+      <header>
+        <div>
+          <img src="img/cv.jpg">
+          <h1 id="top">Michał Ziemichód</h1>
+          <div>
+            <p>Moim celm jest zostać forn-end developer</p>
+          </div>
+          <p id="quotation">"I don't stop when I'm tired. <br>
+             I stop when I done."</p>
+        </div> 
+      </header>
+      <main>
+      <section>
+        <article>
+          <div class="aboutMe">
+            <h1 id="aboutMe">O mnie</h1>
+            <p>Cześć. jestem Michał pochodzę z Miasteczka Krajeńskiego. Obecnie pracuje jako Technolog w firmie produkującej meble tapicerowane. W wolnym czasue zajmuję się nauką technologi webowych.
+            </p>
+          </div>
+        </article>
+      </section>
+      <section>
+        <article>
+          <div class="aboutMe">
+            <h1 id="education">Wykształcenie</h1>
+            <p> 2010 – 2012 </p> 
+            <p>     
+              Akademia  Morska w Szczecinie, Logistyka i Zarządzanie w Europejskich Systemach transportowych. Studia magisterskie
+            </p>
+            <p>
+              2006 – 2010       
+            </p>
+            <p>
+              Akademia  Morska w Szczecinie, Zarządzanie Jakością Produkcji i Transportem Zintegrowanym. Studia inżynierskie
+            </p>
+          </div>
+        </article>
+      </section>
+      <section>
+        <article>
+          <div class="aboutMe">
+            <h1 id="skills">Umiejętności</h1>
+            <ul>
+              <li>HTML5 - ✯✯✩✩✩</li>
+              <li>CSS3 - ✯✯✩✩✩</li>
+              <li>JavaScript - ✯✯✩✩✩</li>
+              <li>jquery - ✯✯✩✩✩</li>
+              <li>Git - ✯✩✩✩✩</li>
+            </ul>
+          </div>
+        </article>
+      </section>
+      <section>
+         <article>
+            <div class="aboutMe">
+            <h1 id="passion">Pasja</h1>
+            <img src="img/run.jpg">
+            <p>
+               Moją pasją jest Triathlon. Zacząłem sie interesować tą dyscypliną po ukonczeniu maratonu. Wtedy za bradzo jeszcze nie wiedziałem z czym to się je. Trochę poczytałem i swierdzialem TO DO DZIEŁA. 
+            </p>
+            <p>
+               Na tym etapie potrafiłem przepłynąc maksymalnie 25m na basenie. A rower jaki miałem to góral który był nie sprawny. 6 miesiecy basenu i jazdy na rowerze pozwoliło mi na ukonczenie pierwszego triathlonu. Satysfakcja z ukonczonych zawodów... ogromna. I tak do dzisiaj ta pasja toważysz mi każdego dnia... 
+            </p>
+            <h2>Wyniki</h2>
+            <ul>
+              <li>1/4 ironman - 3:48h</li>
+              <li>maraton - 3:49h</li>
+              <li>pół maraton - 1:42h</li>
+            </ul>
+          </div>
+          </article>
+      </section>
+      <footer>
+        <div id="contact">
+          <h1>Kontakt</h1>
+          <p>Michał Ziemichód</p>
+          <p>mziemichod@wp.pl</p>
+          <p>579631861</p>
+        </div>
+      </footer>
+      <div id="up"></div>
+    </main>
+    <script type="text/javascript" src="js/jsindex.js"></script>
+</body>
+</html>
